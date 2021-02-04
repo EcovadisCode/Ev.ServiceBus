1785e431fc9c5abf2fde9ae1ecc482cc54db951b
Benjamin SPETH <bspeth@ecovadis.com>
2021-02-04 16:48:50 +0100

    moved some files

---
+++ src/Ev.ServiceBus.IntegrationEvents/Subscription/ReceptionBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ev.ServiceBus.IntegrationEvents.Subscription
+{
+    public class ReceptionBuilder
+    {
+        private readonly IServiceCollection _services;
+
+        public ReceptionBuilder(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public ReceptionRegistrationBuilder FromQueue(string queueName)
+        {
+            if (queueName == null)
+            {
+                throw new ArgumentNullException(nameof(queueName));
+            }
+
+            var options = _services.RegisterServiceBusQueue(queueName)
+                .ToIntegrationEventHandling();
+            return new ReceptionRegistrationBuilder(_services, options);
+        }
+
+        public ReceptionRegistrationBuilder FromSubscription(string topicName, string subscriptionName)
+        {
+            if (topicName == null)
+            {
+                throw new ArgumentNullException(nameof(topicName));
+            }
+
+            if (subscriptionName == null)
+            {
+                throw new ArgumentNullException(nameof(subscriptionName));
+            }
+
+            var options = _services.RegisterServiceBusSubscription(topicName, subscriptionName)
+                .ToIntegrationEventHandling();
+            return new ReceptionRegistrationBuilder(_services, options);
+        }
+    }
+}
