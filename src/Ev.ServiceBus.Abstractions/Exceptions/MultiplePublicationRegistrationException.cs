c14817ef89d200613f775620ce1032557b4dcb74
Benjamin SPETH <benjaminspeth@hotmail.fr>
2021-02-22 15:49:33 +0100

    Added some log messages to the MessageSender (#19)
    Co-authored-by: Benjamin SPETH <bspeth@ecovadis.com>

---
+++ src/Ev.ServiceBus.Abstractions/Exceptions/MultiplePublicationRegistrationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ev.ServiceBus.Abstractions.Exceptions
+{
+    [Serializable]
+    public class MultiplePublicationRegistrationException : Exception
+    {
+        public MultiplePublicationRegistrationException(IReadOnlyList<string> registrationIds)
+        {
+            Message = $"You can't register the same contract more than once.\n"
+                      + $"Registrations at fault : \n"
+                      + $"{string.Join("\n", registrationIds)}";
+        }
+
+        public override string Message { get; }
+    }
+}
--- src/Ev.ServiceBus/Management/Senders/MessageSender.cs
+++ src/Ev.ServiceBus/Management/Senders/MessageSender.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Ev.ServiceBus.Abstractions;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.Extensions.Logging;
 using IMessageSender = Ev.ServiceBus.Abstractions.IMessageSender;
 
 // ReSharper disable once CheckNamespace
@@ -12,10 +14,12 @@ namespace Ev.ServiceBus
     public class MessageSender : IMessageSender
     {
         private readonly ISenderClient _client;
+        private readonly ILogger<MessageSender> _logger;
 
-        public MessageSender(ISenderClient client, string name, ClientType clientType)
+        public MessageSender(ISenderClient client, string name, ClientType clientType, ILogger<MessageSender> logger)
         {
             _client = client;
+            _logger = logger;
             Name = name;
             ClientType = clientType;
         }
@@ -25,21 +29,25 @@ namespace Ev.ServiceBus
 
         public Task SendAsync(Message message)
         {
+            _logger.LogInformation($"[Ev.ServiceBus] Sending a message to {ClientType} {Name}");
             return _client.SendAsync(message);
         }
 
         public Task SendAsync(IList<Message> messageList)
         {
+            _logger.LogInformation($"[Ev.ServiceBus] Sending {messageList.Count()} messages to {ClientType} {Name}");
             return _client.SendAsync(messageList);
         }
 
         public Task<long> ScheduleMessageAsync(Message message, DateTimeOffset scheduleEnqueueTimeUtc)
         {
+            _logger.LogInformation($"[Ev.ServiceBus] Scheduling a message to {ClientType} {Name} to be executed at {scheduleEnqueueTimeUtc:O}");
             return _client.ScheduleMessageAsync(message, scheduleEnqueueTimeUtc);
         }
 
         public Task CancelScheduledMessageAsync(long sequenceNumber)
         {
+            _logger.LogInformation($"[Ev.ServiceBus] Cancelling a scheduled message {ClientType} {Name}");
             return _client.CancelScheduledMessageAsync(sequenceNumber);
         }
     }
