52153fcdbf0358414e8961c1e5f3053f3c4c2c07
Benjamin SPETH <benjaminspeth@hotmail.fr>
2021-03-26 14:12:50 +0100

    Added workflow to publish NuGets upon tag creation (#25)
    * Added workflow to publish NuGets upon tag creation
    
    Co-authored-by: Benjamin SPETH <bspeth@ecovadis.com>

--- src/Ev.ServiceBus/Dispatch/DispatchRegistry.cs
+++ src/Ev.ServiceBus/Dispatch/DispatchRegistry.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Ev.ServiceBus.Abstractions.Exceptions;
+using Ev.ServiceBus.Abstractions;
 
 namespace Ev.ServiceBus.Dispatch
 {
