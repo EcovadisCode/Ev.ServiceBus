fc53f563c25d565f20e8b8e34f0bbda51c008afc
Benjamin SPETH <benjaminspeth@hotmail.fr>
2022-02-18 10:04:59 +0100

    Release/v3.6.0 (#44)
    * Added a way to set the sessionId during the publish (#42)
    
    * Added a way to set the sessionId during the publish
    
    
    Co-authored-by: Benjamin SPETH <bspeth@ecovadis.com>
    
    * CHANGELOG
    
    Co-authored-by: Benjamin SPETH <bspeth@ecovadis.com>

---
+++ src/Ev.ServiceBus.Abstractions/Dispatch.cs
@@ -0,0 +1,13 @@
+﻿namespace Ev.ServiceBus.Abstractions;
+
+public sealed class Dispatch
+{
+    public Dispatch(object payload)
+    {
+        Payload = payload;
+    }
+
+    public object Payload { get; }
+    public string? SessionId { get; set; }
+}
+
